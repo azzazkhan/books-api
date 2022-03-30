bc22c8b021019c4b721893b58507215b2a2f056a
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2022-03-30 23:59:09 +0500

    Created prisma module and service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'mysql://admin@localhost:3306/books_api',
+        },
+      },
+    });
+  }
+}
